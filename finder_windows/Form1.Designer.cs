89771cc98591848760dcf112704b2b6bb1164c95
蔡承祐 <>
2018-10-17 15:00:55 +0800

    初版

---
+++ finder_windows/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace finder_windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.choise_dir = new System.Windows.Forms.Button();
+            this.listboxpanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // choise_dir
+            // 
+            this.choise_dir.Dock = System.Windows.Forms.DockStyle.Top;
+            this.choise_dir.Location = new System.Drawing.Point(0, 0);
+            this.choise_dir.Name = "choise_dir";
+            this.choise_dir.Size = new System.Drawing.Size(800, 30);
+            this.choise_dir.TabIndex = 0;
+            this.choise_dir.Text = "選擇資料夾";
+            this.choise_dir.UseVisualStyleBackColor = true;
+            this.choise_dir.Click += new System.EventHandler(this.choise_dir_Click);
+            // 
+            // listboxpanel
+            // 
+            this.listboxpanel.AutoScroll = true;
+            this.listboxpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listboxpanel.Location = new System.Drawing.Point(0, 30);
+            this.listboxpanel.Name = "listboxpanel";
+            this.listboxpanel.Size = new System.Drawing.Size(800, 420);
+            this.listboxpanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listboxpanel);
+            this.Controls.Add(this.choise_dir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button choise_dir;
+        private System.Windows.Forms.Panel listboxpanel;
+    }
+}
+
---
+++ finder_windows/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace finder_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int id_name = 1;
+        string dirpath = "";
+        private void choise_dir_Click(object sender, EventArgs e)
+        {
+            listboxpanel.Controls.Clear(); //to remove all controls
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            dirpath = path.SelectedPath;
+            auto_listbox("folder_box"+ id_name, dirpath);
+        }
+        public void auto_listbox(string list_box_name,string paths)
+        {
+            ListBox listBox = new ListBox();
+            foreach (string fname in System.IO.Directory.GetFileSystemEntries(paths))
+            {
+                listBox.Items.Add(fname.Replace(paths, ""));
+            }
+            listBox.Name = list_box_name;
+            listBox.Dock = DockStyle.Right;
+            listBox.Width = 150;
+            listBox.ScrollAlwaysVisible = true;
+            listBox.HorizontalScrollbar = true;
+            //add SelectedIndexChanged even
+            listBox.SelectedIndexChanged += listBox_SelectedIndexChanged;
+            listboxpanel.Controls.Add(listBox);
+        }
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox filename = (ListBox)sender;
+            //listboxNames.SelectedItem.ToString()
+            if (Directory.Exists(dirpath + filename.SelectedItem.ToString()))
+            {
+                dirpath = dirpath + filename.SelectedItem.ToString();
+                id_name += 1;
+                auto_listbox("folder_box"+ id_name, dirpath );
+            }
+            else
+            {
+                //利用for迴圈改路徑
+                //並刪除listbox
+                string name = filename.Name;
+                int gap = id_name -int.Parse(name.Replace("folder_box", ""));
+                if (gap != 0)
+                {
+                    string[] splite = dirpath.Split('\\');
+                    for(int k=id_name;k> int.Parse(name.Replace("folder_box", "")); k--)
+                    {
+                        foreach (Control item in listboxpanel.Controls.OfType<ListBox>())
+                        {
+                            if (item.Name == "folder_box" + k)
+                            {
+                                listboxpanel.Controls.Remove(item);
+                                break;
+                            }
+                        }
+                    }
+                    //更新路徑
+                    string new_p = "";
+                    for(int i = 0; i < splite.Length-gap; i++)
+                    {
+                        new_p += splite[i]+"\\";
+                    }
+                    id_name = int.Parse(name.Replace("folder_box", ""));
+                    dirpath = new_p;
+                    listBox_SelectedIndexChanged(sender, e);
+                }
+            }
+        }
+
+    }
+}
